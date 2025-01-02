d150e535120d2cfdfc0d4ba764456f5c52bf0fff
kostia-stepanenko <stepanenko.kotia@gmail.com>
2025-01-02 11:01:38 -0100

    Repository created

---
+++ workspace-service/src/test/java/com/github/kostia/workspace_service/WorkspaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kostia.workspace_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
