79d449df95e177658a94a51c519fc42f5b1df13a
kostia-stepanenko <stepanenko.kotia@gmail.com>
2025-05-03 16:28:32 +0000

    New sql tables added + application.yaml added

--- src/main/resources/db/migration/V2__inserting_tennt_configs.sql
+++ src/main/resources/db/migration/V2__inserting_tennt_configs.sql
@@ -1,8 +1,8 @@
-INSERT INTO TENANT_CONFIG (lifecycleState, version, customerTenancyId)
-VALUES ('ACTIVE', 'v1.0', 101);
+INSERT INTO TENANT_CONFIG (lifecycle_state, version, customer_tenancy_id)
+VALUES ('ACTIVE', 1, '101');
 
-INSERT INTO TENANT_CONFIG (lifecycleState, version, customerTenancyId)
-VALUES ('ACTIVE', 'v1.0', 103);
+INSERT INTO TENANT_CONFIG (lifecycle_state, version, customer_tenancy_id)
+VALUES ('ACTIVE', 1, '103');
 
-INSERT INTO TENANT_CONFIG (lifecycleState, version, customerTenancyId)
-VALUES ('ACTIVE', 'v1.0', 104);
+INSERT INTO TENANT_CONFIG (lifecycle_state, version, customer_tenancy_id)
+VALUES ('ACTIVE', 1, '104');
