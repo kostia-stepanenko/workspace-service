7b4418fdb9d90f6aa3b88942059ee472e4581a78
kostia-stepanenko <stepanenko.kotia@gmail.com>
2025-05-08 17:22:44 +0000

    merged getTenantByCustomerId and getAllTenant into single endpoint, had to slightly change TenantConfigRepo (Not sure how important it is?)

--- src/main/java/com/github/kostia/workspace_service/tenant/TenantConfigRepo.java
+++ src/main/java/com/github/kostia/workspace_service/tenant/TenantConfigRepo.java
@@ -2,8 +2,6 @@ package com.github.kostia.workspace_service.tenant;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface TenantConfigRepo extends CrudRepository<TenantConfig, Integer> {
-    Optional<TenantConfig> findByCustomerTenancyId(String customerTenancyId);
+    Iterable<TenantConfig> findByCustomerTenancyId(String customerTenancyId);
 }
